460a28788aa610ca560f0e71560aa2e833ec87ad
elek8871 <elekmichelle@gmail.com>
2022-09-22 20:57:49 -0600

    completes part one of deliverable

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React from "react"
+import  ReactDOM  from "react-dom"
+import MyList from "./MyList"
+import "./index.css"
+
+const toDos = ["sleep", "code", "relax"]
+
+ReactDOM.render(
+  <MyList theList={toDos} />,
+  document.getElementById("root")
+)
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
